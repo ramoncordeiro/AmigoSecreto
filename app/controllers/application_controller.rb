5e9e07ab67639827bc333fff9a2d57427dea0144
ramoncord <ramonestii@hotmail.com>
2018-07-16 10:35:46 -0300

    rescue from 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  def render_404
+    redirect_to main_app.root_url # Quando der um erro. Ele vai mandar pra pagina inicial
+  end
 
 end
